a56b6384387f8195b1a88ce8207c7eb8d77b6a31
Nathan L. Sickler <nlsickler@sep.com>
2025-08-06 13:14:27 -0400

    Add Watch module with Watch-Command functionality
    - Created PSGetModuleInfo.xml for module metadata.
    - Implemented Watch-Command.ps1 to monitor scriptblock output changes.
    - Added module manifest Watch.psd1 for module configuration.
    - Created Watch.psm1 to load public functions and set aliases.
    - Updated nlsicklerTheme.omp.json for improved prompt styling and functionality.

---
+++ Modules/Watch/1.0.31/Watch.psm1
@@ -0,0 +1,21 @@
+﻿$Public = @( Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -Recurse )
+
+$Public | ForEach-Object {
+    try {
+        . $_.FullName
+    }
+    catch {
+        Write-Error -Message "Failed to import function $($_.FullName): $_"
+    }
+}
+
+if (-not (Test-Path alias:Watch)) {
+    New-Alias -Name 'Watch' -Value 'Watch-Command'
+    Export-ModuleMember -Function $Public.BaseName -Alias 'Watch'
+}
+
+if (-not (Test-Path alias:wc)) {
+    New-Alias -Name 'wc' -Value 'Watch-Command'
+    Export-ModuleMember -Function $Public.BaseName -Alias 'wc'
+}
+
